e13f7c9376e84bd032a08a0e23b6c521a1d34571
Luka Koridze <luka.qoridze2002@gmail.com>
2022-09-28 17:02:30 +0400

    Desktop UI Finished

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,14 @@
 import Data from '../Data/data.js'
 let domains = Data.domainList;
 let categories = Data.categories;
+let domainZones = [];
+
+// Creating available domains list
+domains.forEach((dom) => {
+    if (!domainZones.includes(dom.domainExtension)) {
+        domainZones.push(dom.domainExtension);
+    }
+})
 
 // Domains on the market
 let domainCount = document.querySelector('.domenebiMarketze span')
@@ -9,7 +17,7 @@ domainCount.innerText = domains.length
 // Sorting
 let sorters = document.querySelectorAll('.sorters p')
 let sortImage = document.createElement('img')
-sortImage.style.marginLeft='5px'
+sortImage.style.marginLeft = '5px'
 sortImage.src = '../Images/sort.svg'
 sorters.forEach((option) => {
     option.addEventListener('click', (e) => {
@@ -23,3 +31,82 @@ sorters.forEach((option) => {
         option.appendChild(sortImage)
     })
 })
+
+
+// Categories
+let categoriesDiv = document.querySelector('.categoriesDiv')
+categories.forEach((category) => {
+    let box = document.createElement('div')
+    box.style.display='flex'
+    box.style.alignItems='center'
+    box.style.marginTop = '25px'
+    let checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.name = category.name
+    checkbox.id = category.name
+    let label = document.createElement('label')
+    label.setAttribute('for', category.name)
+    label.innerText = category.name
+    box.appendChild(checkbox)
+    box.appendChild(label)
+    categoriesDiv.appendChild(box)
+})
+
+// Domain Zones
+let domainZone = document.querySelector('.domainZones')
+domainZones.forEach((zone) => {
+    let box = document.createElement('div')
+    box.style.display = 'flex'
+    box.style.alignItems = 'center'
+    box.style.marginTop='25px'
+    let checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.name = zone
+    checkbox.id = zone
+    let label = document.createElement('label')
+    label.setAttribute('for', zone)
+    label.innerText = zone
+    box.appendChild(checkbox)
+    box.appendChild(label)
+    domainZone.appendChild(box)
+})
+
+// Products
+let parent = document.querySelector('.middle .right')
+domains.forEach((product,index)=>{
+    let card = document.createElement('div');
+    let left = document.createElement('div');
+    let right = document.createElement('div');
+    let img = document.createElement('img');
+    img.src='../Images/btn_dropdown.svg'
+    let name= document.createElement('p');
+    name.innerText = product.domainName+product.domainExtension;
+    let priceGel = document.createElement('p');
+    priceGel.innerText = product.price + " ₾";
+    let priceUsd = document.createElement('p');
+    priceUsd.innerText = Math.round(product.price / 2.85 * 100) / 100 + " $";
+    let div = document.createElement('div');
+    let cart = document.createElement('img');
+    cart.src = '../Images/cart_white.svg';
+
+
+    // Appends
+    parent.appendChild(card)
+    card.appendChild(left);
+    card.appendChild(right);
+    left.appendChild(img);
+    left.appendChild(name);
+    right.appendChild(div);
+    right.appendChild(cart);
+    div.appendChild(priceGel);
+    div.appendChild(priceUsd);
+    
+    // CLassing
+    card.classList.add('cardModel');
+    left.classList.add('leftCard');
+    right.classList.add('rightCard');
+    priceGel.classList.add('gel');
+    priceUsd.classList.add('usd');
+
+})
+
